8e486eac4ff85dfafb27dc5f9eb2af890d81226e
br3-mah <nyeleti.bremah@gmail.com>
2023-05-09 12:32:43 +0200

    fixed email SMPT

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
                 <div class="p-3" style="max-width: 900px;">
                     <h5 class="text-white text-uppercase mb-3 animated slideInDown">Let us do the dirty work for you</h5>
                     <h1 class="display-1 text-white mb-md-4 animated zoomIn">Healthy Homes Start with Clean Spaces</h1>
-                    <a  href="#facleaners-quote" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
-                    <a href="" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
+                    <a href="#facleaners-quote" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
+                    <a href="{{ route('contact-us') }}" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
                 </div>
             </div>
         </div>
@@ -19,8 +19,8 @@
                 <div class="p-3" style="max-width: 900px;">
                     <h5 class="text-white text-uppercase mb-3 animated slideInDown">Clean space, happy place</h5>
                     <h1 class="display-1 text-white mb-md-4 animated zoomIn">Cleaning That Fits Your Schedule</h1>
-                    <a onclick="startRating()" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
-                    <a href="#facleaners-quote" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
+                    <a href="#facleaners-quote" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
+                    <a href="{{ route('contact-us') }}" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
                 </div>
             </div>
         </div>
