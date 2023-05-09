9fed60f92d50d396f74075369db1388332840894
br3-mah <nyeleti.bremah@gmail.com>
2023-05-09 12:22:30 +0200

    fixed email SMPT

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -31,7 +31,7 @@ class ContactUsController extends Controller
     {
         $data = $request->toArray();
         // $to_email = 'nyeleti.bremah@gmail.com';
-        $to_email = 'Info@facleaners.ca';
+        $to_email = 'info@facleaners.ca';
 
         // replace with the content of the email
         $content = [
